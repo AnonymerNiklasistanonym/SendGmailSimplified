988dc482c3f74ad66c0f1c9845daecef91851304
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-08-17 01:43:39 +0200

    Inital upload of working files

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # SendGmailSimplified
 Send with only 3 lines of python code a email over the official Gmail API. Easy to add and simple to use.
+
+
+
+## What is this API?
+
+This python API is nothing new or groundbreaking. I just wanted to send an email with the official Gmail API with nothing but 2 Lines in the code and one import in python.
+
+This could make and made my python `Cron` job scripts, which check different things over the day really effective, because I can now very easy integrate the Gmail API and send notification or emails within one line anywhere in the script.
+
+I connected with some people and read the official Google Documentation and... now you and everyone can use an optimize it (MIT license).
+
+Have fun!
+
+
+
+## Instructions
+
+(Source: [Google Developers Gmail API Python](https://developers.google.com/gmail/api/quickstart/python))
+
+### 0. Create or Have a Google account
+
+Obviously you first need a Google account and logged into Gmail for once to set up everything.
+
+### 1. Google Developer Console setup
+
+Visit the [Google API website](https://console.developers.google.com/start/api?id=gmail) and create a new Project:
+
+- Click `Select a project` at the top left (after you logged in with your Google account)
+  - Then named it and press continue on the next page
+- After some time the API is activated and a button `Go to credentials` will appear
+  - Simple: Click `Cancel` at the bottom of the page
+- Now you will find yourself in a tab interface
+  - Click the tab which is named `OAuth consent screen`
+    - Select the right email address
+    - Enter the name of your project/service/product
+    - You can do more but for now you are ready and can press `Save`
+  - Back in the tab interface click the tab which is named `Credentials`
+    - Now we want to create credentials and therefore expand the button `Create credentials` by clicking the triangle
+    - Now let's click the entry `OAuth client ID`
+      - Choose there your application type
+        (I chose `Other` and entered the name of my service + "ClientID")
+    - Now a small window will pop up which we will instantly dismiss
+      (by clicking `OK`)
+  - But because we want the client ID we'll download the JSON file of the created client ID:
+    - Just click the arrow down next to the client ID entry (:arrow_down:)
+
+Now you have the necessary file with which we can set up the Gmail API.
+
+### 2. Set up the Gmail API
+
+The following steps will be really easy:
+
+- Rename the downloaded JSON file to `client_secret.json`
+
+Copy the file to a desired directory of yourself.
+
+Create another JSON file (just copy the following code in a text file and rename it `client_data.json`):
+
+```json
+{
+	"email": "yourMailAdresse@gmail.com",
+	"email-name": "Your Name",
+	"application-name": "Previous created service name",
+	"permission-scope": "https://www.googleapis.com/auth/gmail.send"
+}
+```
+
+Edit the file as following:
+
+- replace the placeholders for everything but the `permission-scope` line with your data
+- copy the file to another desired directory of yourself
+  (if you want it simple put it in the same directory)
+
+### 3. Activate the API in your python script
+
+This is even simpler.
+
+You now just need three lines to activate and test the API:
+
+```python
+from SendGmailSimplified import SimplifiedGmailApi
+
+GmailServer = SimplifiedGmailApi("gmail_api_files/client_data.json", "gmail_api_files/client_secret.json", "gmail_api_files")
+GmailServer.send_plain("yourEmailAddress@gmail.com", "Test-Subject", "1,2,3,4...\nTest, test")
+```
+
+The three paths are:
+
+| Path to `client_data.json`           | Path to `client_secret.json`           | Directory of [future] API file |
+| ------------------------------------ | -------------------------------------- | ------------------------------ |
+| `"gmail_api_files/client_data.json"` | `"gmail_api_files/client_secret.json"` | `"gmail_api_files"`            |
+
+Now execute the script.
+
+- A browser window should open itself and ask you to login with your Google account
+- Then allow the API to `Send email on your behalf` over your account
+
+After you clicked `Allow` the necessary file is automatically saved into the directory on your computer of your third parameter.
+
+If you didn't get an email look in the console.
+Probably you find something like `403 - Message could not be send`.
+
+But this is not a problem: Just click like the official Gmail API says on the link just to get forwarded back to the API Developer Console.
+Then you do what the text says (at least for me) and click `Enable`.
+
+
+
+## Problems, ideas?
+
+- If this didn't worked for you message me, make an Issue and we try to help if we have time.
+- If you have any ideas, optimizations please message me too (or open a issue/pull-request).
+
+
+
+## Last words
+
+We hope this API simplifies sending an email to someone with the official Gmail API without any headaches and many lines of code.
+
+Thanks for using this simple API and have fun programming!
---
+++ SendGmailSimplified.py
@@ -0,0 +1,171 @@
+from __future__ import print_function
+import httplib2
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import base64
+from email.mime.text import MIMEText
+import os
+from apiclient import errors
+import argparse
+import logging
+import json
+
+
+class SimplifiedGmailApi:
+
+    def __init__(self, a=None, b=None, c=os.path.join(os.path.expanduser('~'), "GmailCredentialsApi")):
+        path_for_gmail_data = a
+        # OwnFile: "email","application-name","permission-scope"
+
+        self.PATH_FOR_GMAIL_CLIENT_SECRET_FILE = b
+        # GmailFile
+        self.DIRECTORY_FOR_GMAIL_API = c
+        # directory for permission thing
+
+        if a is None or b is None or c is None:
+            print("Something went wrong!!! API will not work!")
+            self.SetupOk = False
+        else:
+            self.SetupOk = True
+
+            with open(path_for_gmail_data, "r") as credentials_file:
+                data_file = json.load(credentials_file)
+
+            try:
+                self.flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+            except ImportError:
+                self.flags = None
+
+            self.SCOPES = data_file["permission-scope"]
+            self.APPLICATION_NAME = data_file["application-name"]
+            self.EMAIL_ADDRESS = data_file["email"]
+            self.EMAIL_ADDRESS_NAME = data_file["email-name"] + " <" + self.EMAIL_ADDRESS + ">"
+
+            credentials = self.__get_credentials()
+            http = credentials.authorize(httplib2.Http())
+            self.service = discovery.build('gmail', 'v1', http=http)
+
+    def send_plain(self, b="to", c="subject", d="message"):
+        return self.__send_mail(b, c, d)
+
+    def send_html(self, b="to", c="subject", d="message"):
+        return self.__send_mail(b, c, d, True)
+
+    def __send_mail(self, b="to", c="subject", d="message", html_mail=False):
+
+        if self.SetupOk:
+
+            print("Send email from " + self.EMAIL_ADDRESS_NAME + " to: " + b + ":")
+            print("          " + c + " (" + ("html" if html_mail else "plain text") + ")")
+            i = 1
+            for x in d.split('\n'):
+                print("     " + "{0:04}".format(i) + " " + x)
+                i += 1
+
+            # create message
+            if html_mail:
+                message = self.__create_message(b, c, d, True)
+            else:
+                message = self.__create_message(b, c, d)
+
+            # send the message
+            if self.__send_message(message) is not None:
+                print("- successfully send to " + b)
+                logging.info("email was successfully send to " + b)
+                return True
+            else:
+                print("WARNING - Message could not be send to " + b)
+                logging.warning("email was not send to " + b)
+                return False
+        else:
+            print("Something went wrong in the setup. Please check your data.")
+
+            return False
+
+    """
+    If you want to implement the Gmail API read this first: https://developers.google.com/gmail/api/quickstart/python
+    You first need to activate the API over a Gmail account, then create OAuth client ID credentials and then
+    understand (run) the example because of the permission scopes (https://developers.google.com/gmail/api/auth/scopes).
+    """
+
+    def __send_message(self, message):
+        """Send an email message.
+
+        Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+        Returns:
+        Sent Message.
+        """
+        try:
+            message = (self.service.users().messages().send(userId="me", body=message).execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
+            return None
+
+    def __create_message(self, to, subject, message_text, message_html=False):
+        """Create a message for an email.
+
+        Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+        Returns:
+        An object containing a base64url encoded email object.
+        """
+        # message_text.encode('us-ascii', 'ignore').decode('us-ascii', 'ignore')
+
+        if message_html:
+            message = MIMEText(message_text, 'html')
+        else:
+            message = MIMEText(message_text)
+
+        message['to'] = to
+        message['from'] = self.EMAIL_ADDRESS_NAME
+        message['subject'] = subject
+        print(message)
+        # print(message.as_string())
+        # print({'raw': base64.urlsafe_b64encode(message.as_string()).encode()})
+        # return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+        raw = base64.urlsafe_b64encode(message.as_bytes())
+        raw = raw.decode()
+        body = {'raw': raw}
+
+        return body
+
+
+
+    def __get_credentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+
+        credential_dir = self.DIRECTORY_FOR_GMAIL_API
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir, 'gmail_api_.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.PATH_FOR_GMAIL_CLIENT_SECRET_FILE, self.SCOPES)
+            flow.user_agent = self.APPLICATION_NAME
+            if self.flags:
+                credentials = tools.run_flow(flow, store, self.flags)
+            print('Storing credentials to ' + credential_path)
+        return credentials
