2b4f5df507de3283afd4f0b379d3362a9203b634
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-08-17 19:22:36 +0200

    Added new demo
    It contains a quick overview about how to send emails with or without attachments and in HTML or plain text.

---
+++ demo.py
@@ -0,0 +1,23 @@
+from SendGmailSimplified import SimplifiedGmailApi
+
+# send for demo uses emails to you or someone other:
+yourEmailAddress = "niklas.mikeler@gmail.com"
+
+# replace the placeholders and enter your paths like the README.md says
+DemoServer = SimplifiedGmailApi("gmail_api_files/client_data.json", "gmail_api_files/client_secret.json", "gmail_api_files")
+
+# Send a plain text message
+DemoServer.send_plain(yourEmailAddress, "Test-Subject", "1,2,3,4...\nTest, test")
+
+# Send a HTML text message
+DemoServer.send_plain(yourEmailAddress, "Test-Subject", "<html><body>1,2,3,4...\nTest, test</body></html>")
+
+# Enter a path to a file (< 25mb) that you want to attach
+attachment = "SendGmailSimplified.py"
+# Send a plain text message with attachments
+DemoServer.send_plain_with_attachment(yourEmailAddress, "Test-Plain-With-Attachment", "1,2,3,4...\nTest, test", attachment)
+
+# Enter paths to files (< 25mb) that you want to attach
+attachments = ["test.m4a", "beta.txt", "test.py", "SendGmailSimplified.py"]
+# Send a HTML text message with attachments
+DemoServer.send_html_with_attachments(yourEmailAddress, "Test-HTML-With-Attachments", "<html><body>1,2,3,4...\nTest, test</body></html>", attachments)
--- gmail_test_script.py
+++
@@ -1,5 +0,0 @@
-from SendGmailSimplified import SimplifiedGmailApi
-
-# replace the placeholders and enter your paths like the README.md says
-GmailServer = SimplifiedGmailApi("gmail_api_files/client_data.json", "gmail_api_files/client_secret.json", "gmail_api_files")
-GmailServer.send_plain("yourEmailAddress@gmail.com", "Test-Subject", "1,2,3,4...\nTest, test")
